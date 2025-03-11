43b86f51f4e06ccbe783b2747eef9145c901730a
Rafael Rojas Diaz <118632412+Raghart@users.noreply.github.com>
2025-03-11 10:16:36 -0400

    Update build_step.sh

--- build_step.sh
+++ build_step.sh
@@ -4,3 +4,6 @@ echo "Build script"
 
 - name: Trigger deployment
   run: curl https://api.render.com/deploy/srv-${{ secrets.RENDER_SERVICE_ID }}?key=${{ secrets.RENDER_API_KEY }}
+
+- name: Install dependencies
+  run: npm install
